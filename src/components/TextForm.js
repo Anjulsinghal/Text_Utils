6fbf9ae27a7b701f2d68f5724e4d56ae8a1d7bf3
Anjulsinghal <95030018+Anjulsinghal@users.noreply.github.com>
2024-07-31 10:27:09 +0530

    Update TextForm.js
    spelling mistakes

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -31,16 +31,16 @@ export default function TextForm(props) {
 
             <h1 className='my-3'>Enter the Text below to analize</h1>
             <textarea className="form-control" value={text} onChange={handelOnChange} style={{backgroundColor: props.mode=== "light"?"white":"#371f60", color: props.mode=== "dark"?"white":"black"}} id="exampleFormControlTextarea1" rows="8"></textarea>
-            <button disabled={text.length===0} className="btn btn-primary  my-2" onClick={handelLowClick}>click to uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary  my-2" onClick={handelLowClick}>click to Lowercase</button>
             <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handelCopy}>Copy text</button>
-            <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handelUpClick}>click to uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handelUpClick}>click to Uppercase</button>
             <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handelSpaces}>Remover Extra Spaces</button>
         </div>
         <div className="container">
             <h1>Your Text Summary</h1>
             <h4>Words: {text.split(/\s+/).filter((element)=> {return element.length!==0}).length} Charcter:{text.length}</h4>
             <h4>{0.008*60 * text.split(" ").filter((element)=> {return element.length!==0}).length} sec take to read that you write</h4>
-            <h2>Priview</h2>
+            <h2>Preview</h2>
             <p>{text.length>0?text:"Enter text in the above textarea to see the preview of Your text"}</p>
         </div>
         </div>
